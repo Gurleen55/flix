8b127782e6ffa5a171de2b140c63508782eeca53
Gurleen55 <gurleencan6@gmail.com>
2025-05-26 18:46:49 -0300

    linking pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root "movies#index"
   get "movies" => "movies#index", as: :movies
+  get "movies/:id" => "movies#show", as: :movie
 end

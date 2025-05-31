e8f73d706199335ecf6482e24d84cb9e3cdb49bc
Gurleen55 <gurleencan6@gmail.com>
2025-05-30 23:13:03 -0300

    one-to-many

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reviews
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "movies#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_30_013501) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_30_171444) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -34,4 +34,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_30_013501) do
     t.string "image_file_name", default: "placeholder.png"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.integer "stars"
+    t.text "comment"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  add_foreign_key "reviews", "movies"
 end

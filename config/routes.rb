4776a66942fd212801a445c5ebab9dae871a9765
Gurleen55 <gurleencan6@gmail.com>
2025-06-06 21:17:19 -0300

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   
   get "up" => "rails/health#show", as: :rails_health_check
-
+  
   root "movies#index"
   
   resources :movies do
     resources :reviews
   end
 
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
+  
+  resources :users
+  get "signup" => "users#new"
 end
